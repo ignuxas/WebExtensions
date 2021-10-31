407823380615b1343dd2d49430d2e2bbe1fb8902
Ignas Mikolaitis <80627100+ignuxas@users.noreply.github.com>
2021-10-31 17:07:15 +0200

    Update Source.js

--- Europe Shipping Extension/Source.js
+++ Europe Shipping Extension/Source.js
@@ -3,15 +3,16 @@
 
 function getthing()
 {
-var ship = ["SPAIN", "Poland", "France", "Czech Republic", "Italy", "GERMANY", "DE"];
-var countrys = document.querySelectorAll('.sku-property-item')
-for(var i = 0; i < ship.length; i++){
-    for(var j = 0; j < countrys.length; j++){
-        var yeet = countrys[j].firstChild.firstChild.innerHTML.includes(ship[i]);
-        if(yeet == true){
-            countrys[j].style.backgroundColor="green";
+    var ship = ["SPAIN", "Poland", "France", "Czech Republic", "Italy", "GERMANY", "DE", "Belgium"];
+    var countrys = document.querySelectorAll('.sku-property-item')
+    for(var i = 0; i < ship.length; i++){
+        for(var j = 0; j < countrys.length; j++){
+            var isEU = countrys[j].firstChild.innerHTML.includes(ship[i]);
+            if(isEU == true){
+                countrys[j].style.backgroundColor="green";
+                countrys[j].firstChild.firstChild.style.color="white";
+            }
         }
-    }
     }
 }
 getthing()
